00a36d07f17e5af795c996f3cd28433a9b76d22e
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-08-30 18:33:41 -0700

    Completed function for insertionSort

--- Sorting_exercise/insertion.js
+++ Sorting_exercise/insertion.js
@@ -1,3 +1,28 @@
-function insertionSort() {}
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let j = i;
+        while (j > 0 && arr[j] < arr[j - 1]) {
+            [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
+            j--;
+        }
+    }
+    console.log("Sorting array:", arr);
+    return arr;
+}
+
+console.log(insertionSort([4, 20, 12, 10, 7, 9])); // [4, 7, 9, 10, 12, 20]
+console.log(insertionSort([0, -10, 7, 4])); // [-10, 0, 4, 7]
+console.log(insertionSort([1, 2, 3])); // [1, 2, 3]
+console.log(insertionSort([]));
+
+let nums = [
+    4, 3, 5, 3, 43, 232, 4, 34, 232, 32, 4, 35, 34, 23, 2,
+    453, 546, 75, 67, 4342, 32
+];
+
+insertionSort(nums)
+
+
+
 
 module.exports = insertionSort;
